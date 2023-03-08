f2f76c55b716b35aabbb89fc1025655af82c54bd
laurapetrie <80486626+laurapetrie@users.noreply.github.com>
2023-03-08 13:53:48 +0000

    Integrated the photo api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,25 +2,34 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [results, setResults] = useState(null);
+  const [photos, setPhotos] = useState(null);
   const apiKey = "e7cba0f4344b9ae720f19t5d48co46c3";
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePhotosResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let apiPhotosUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(apiPhotosUrl).then(handlePhotosResponse);
   }
 
   return (
@@ -36,6 +45,7 @@ export default function Dictionary() {
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
